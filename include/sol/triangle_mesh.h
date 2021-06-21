20e2df69397ab320f726e18e99d7c218b7107ddb
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2021-06-21 18:51:53 +0200

    Improve ray-tracing performance

--- include/sol/triangle_mesh.h
+++ include/sol/triangle_mesh.h
@@ -30,12 +30,6 @@ public:
     /// Returns the number of triangles in the mesh.
     size_t triangle_count() const { return indices_.size() / 3; }
 
-    /// Returns the triangle located at the given triangle index.
-    proto::Trianglef triangle_at(size_t triangle_index) const {
-        auto [i0, i1, i2] = triangle_indices(triangle_index);
-        return proto::Trianglef(vertices_[i0], vertices_[i1], vertices_[i2]);
-    }
-
     /// Returns the vertex indices of a triangle located a given triangle index.
     std::tuple<size_t, size_t, size_t> triangle_indices(size_t triangle_index) const {
         return std::tuple {
@@ -45,18 +39,17 @@ public:
         };
     }
 
-    const std::vector<proto::Vec3f>& vertices()   const { return vertices_;  }
     const std::vector<proto::Vec3f>& normals()    const { return normals_; }
     const std::vector<proto::Vec2f>& tex_coords() const { return tex_coords_; }
-
-    const std::vector<const Bsdf*>& bsdfs() const { return bsdfs_; }
+    const std::vector<const Bsdf*>&  bsdfs()      const { return bsdfs_; }
 
 private:
     struct BvhData;
-    std::unique_ptr<BvhData> build_bvh() const;
+    std::unique_ptr<BvhData> build_bvh(const std::vector<proto::Vec3f>&) const;
+    std::vector<proto::PrecomputedTrianglef> build_triangles(const std::vector<proto::Vec3f>&) const;
 
     std::vector<size_t> indices_;
-    std::vector<proto::Vec3f> vertices_;
+    std::vector<proto::PrecomputedTrianglef> triangles_;
     std::vector<proto::Vec3f> normals_;
     std::vector<proto::Vec2f> tex_coords_;
     std::vector<const Bsdf*> bsdfs_;
--- src/triangle_mesh.cpp
+++ src/triangle_mesh.cpp
@@ -38,27 +38,24 @@ TriangleMesh::TriangleMesh(
     std::vector<const Bsdf*>&& bsdfs,
     std::unordered_map<size_t, const TriangleLight*>&& lights)
     : indices_(std::move(indices))
-    , vertices_(std::move(vertices))
     , normals_(std::move(normals))
     , tex_coords_(std::move(tex_coords))
     , bsdfs_(std::move(bsdfs))
     , lights_(std::move(lights))
 {
-    bvh_data_ = build_bvh();
+    bvh_data_ = build_bvh(vertices);
+    triangles_ = build_triangles(vertices);
 }
 
 TriangleMesh::~TriangleMesh() = default;
 
 std::optional<Hit> TriangleMesh::intersect_closest(proto::Rayf& ray) const {
-    using HitInfo = std::tuple<size_t, proto::Vec3f, float, float>;
     auto hit_info = bvh::SingleRayTraverser<Bvh>::traverse<false>(ray, bvh_data_->bvh,
         [&] (proto::Rayf& ray, const Bvh::Node& leaf) {
-            std::optional<HitInfo> hit_info;
-            for (size_t i = 0; i < leaf.prim_count; ++i) {
-                auto triangle_index = bvh_data_->bvh.prim_indices[leaf.first_index + i];
-                auto triangle = triangle_at(triangle_index);
-                if (auto uv = triangle.intersect(ray))
-                    hit_info = std::make_optional(HitInfo { triangle_index, triangle.raw_normal(), uv->first, uv->second });
+            std::optional<std::tuple<size_t, float, float>> hit_info;
+            for (size_t i = leaf.first_index, n = i + leaf.prim_count; i < n; ++i) {
+                if (auto uv = triangles_[i].intersect(ray))
+                    hit_info = std::make_optional(std::tuple { i, uv->first, uv->second });
             }
             return hit_info;
         });
@@ -66,8 +63,11 @@ std::optional<Hit> TriangleMesh::intersect_closest(proto::Rayf& ray) const {
     if (!hit_info)
         return std::nullopt;
 
-    auto [triangle_index, face_normal, u, v] = *hit_info;
-    auto [i0, i1, i2] = triangle_indices(triangle_index);
+    auto [permuted_index, u, v] = *hit_info;
+    auto face_normal    = triangles_[permuted_index].normal();
+    auto triangle_index = bvh_data_->bvh.prim_indices[permuted_index];
+    auto [i0, i1, i2]   = triangle_indices(triangle_index);
+
     auto normal     = proto::lerp(normals_[i0], normals_[i1], normals_[i2], u, v);
     auto tex_coords = proto::lerp(tex_coords_[i0], tex_coords_[i1], tex_coords_[i2], u, v);
 
@@ -76,7 +76,7 @@ std::optional<Hit> TriangleMesh::intersect_closest(proto::Rayf& ray) const {
     surf_info.point         = ray.point_at(ray.tmax);
     surf_info.tex_coords    = tex_coords;
     surf_info.surf_coords   = proto::Vec2f(u, v);
-    surf_info.face_normal   = proto::normalize(face_normal);
+    surf_info.face_normal   = face_normal;
     surf_info.local         = proto::ortho_basis(proto::normalize(normal));
 
     const Light* light = nullptr;
@@ -87,17 +87,17 @@ std::optional<Hit> TriangleMesh::intersect_closest(proto::Rayf& ray) const {
 
 bool TriangleMesh::intersect_any(const proto::Rayf& init_ray) const {
     auto ray = init_ray;
-    return static_cast<bool>(bvh::SingleRayTraverser<Bvh>::traverse<true>(ray, bvh_data_->bvh,
-        [&] (proto::Rayf& ray, const Bvh::Node& leaf) -> std::optional<std::tuple<>> {
-            for (size_t i = 0; i < leaf.prim_count; ++i) {
-                if (triangle_at(bvh_data_->bvh.prim_indices[leaf.first_index + i]).intersect(ray))
-                    return std::make_optional(std::make_tuple());
+    return bvh::SingleRayTraverser<Bvh>::traverse<true>(ray, bvh_data_->bvh,
+        [&] (proto::Rayf& ray, const Bvh::Node& leaf) {
+            for (size_t i = leaf.first_index, n = i + leaf.prim_count; i < n; ++i) {
+                if (triangles_[i].intersect(ray))
+                    return true;
             }
-            return std::nullopt;
-        }));
+            return false;
+        });
 }
 
-std::unique_ptr<TriangleMesh::BvhData> TriangleMesh::build_bvh() const {
+std::unique_ptr<TriangleMesh::BvhData> TriangleMesh::build_bvh(const std::vector<proto::Vec3f>& vertices) const {
     auto bboxes  = std::make_unique<proto::BBoxf[]>(triangle_count());
     auto centers = std::make_unique<proto::Vec3f[]>(triangle_count());
 
@@ -108,7 +108,10 @@ std::unique_ptr<TriangleMesh::BvhData> TriangleMesh::build_bvh() const {
         range.begin(), range.end(), proto::BBoxf::empty(),
         [] (proto::BBoxf left, const proto::BBoxf& right) { return left.extend(right); },
         [&] (size_t i) {
-            auto triangle = triangle_at(i);
+            auto triangle = proto::Trianglef(
+                vertices[indices_[i * 3 + 0]],
+                vertices[indices_[i * 3 + 1]],
+                vertices[indices_[i * 3 + 2]]);
             auto bbox = triangle.bbox();
             bboxes[i]  = bbox;
             centers[i] = triangle.center();
@@ -120,6 +123,20 @@ std::unique_ptr<TriangleMesh::BvhData> TriangleMesh::build_bvh() const {
     auto bvh = Builder::build(scheduler, global_bbox, bboxes.get(), centers.get(), triangle_count());
     bvh::SequentialReinsertionOptimizer<Bvh>::optimize(bvh);
     return std::make_unique<BvhData>(std::move(bvh));
+}
+
+std::vector<proto::PrecomputedTrianglef> TriangleMesh::build_triangles(const std::vector<proto::Vec3f>& vertices) const {
+    auto range = std::views::iota(size_t{0}, triangle_count());
+    std::vector<proto::PrecomputedTrianglef> triangles(triangle_count());
+    std::for_each(std::execution::par_unseq, range.begin(), range.end(),
+        [&] (size_t i) {
+            auto j = bvh_data_->bvh.prim_indices[i];
+            triangles[i] = proto::PrecomputedTrianglef(
+                vertices[indices_[j * 3 + 0]],
+                vertices[indices_[j * 3 + 1]],
+                vertices[indices_[j * 3 + 2]]);
+        });
+    return triangles;
 }
 
 } // namespace sol
